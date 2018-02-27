e81fcd86a9d2bb0b9e3bd6729fb82850b79e4dbb
Igneous01 <5571589+Igneous01@users.noreply.github.com>
2018-02-27 03:02:02 -0500

    Set Controllers to return application/json

--- KIAPI/App_Start/WebApiConfig.cs
+++ KIAPI/App_Start/WebApiConfig.cs
@@ -19,6 +19,13 @@ namespace KIAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+            .Add(new System.Net.Http.Formatting.RequestHeaderMapping("Accept",
+                                          "text/html",
+                                          StringComparison.InvariantCultureIgnoreCase,
+                                          true,
+                                          "application/json"));
         }
     }
 }
